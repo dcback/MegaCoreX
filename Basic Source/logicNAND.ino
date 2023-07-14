ebe8aa67a9825b9db5cd2e57a672b4db15713893
DC Back <45322614+dcback@users.noreply.github.com>
2023-07-14 15:43:01 +0900

    Add files via upload

---
+++ Basic Source/logicNAND.ino
@@ -0,0 +1,37 @@
+// Test Condition : D7@PA7 -- D8@PC0
+
+#include <Logic.h>
+#define ledPin 7
+
+bool state = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  // Initialize logic block 2
+  // Logic block 2 has three inputs, PD0, PD1 and PD2.
+  // It has one output, but this is disabled because we're using an interrupt instead.
+  Logic1.enable = true;                     // Enable logic block 2
+  Logic1.input0 = logic::in::input;         // Set PC0 as input
+  Logic1.input1 = logic::in::input_pullup;  // Set PC1 as input with pullup
+  Logic1.output = logic::out::enable;       // Enable logic block 2 output pin (PC3)
+  Logic1.filter = logic::filter::disable;   // No output filter enabled
+  Logic1.truth = 0xF7;                      // Set AND truth table
+
+  // Initialize logic block 2
+  Logic1.init();
+
+  // Start the AVR logic hardware
+  Logic::start();
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+
+  Serial.print(digitalRead(8));
+  Serial.print("\t");
+  Serial.println(digitalRead(11));
+  state = !state;
+  delay(1000);
+}
