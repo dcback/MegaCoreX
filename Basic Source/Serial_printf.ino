6d965ff86853e8263d3c60c588983103a037240f
DC Back <45322614+dcback@users.noreply.github.com>
2023-07-18 05:38:10 +0900

    Update Serial_printf.ino

--- Basic Source/Serial_printf.ino
+++ Basic Source/Serial_printf.ino
@@ -1,14 +1,23 @@
 // Serial.printf Function
-int ch = 65;
+unsigned int ch = 65;
 
 void setup() {
   Serial.begin(115200);
+
+  Serial.println("Serial.printf vs Serial.print");
 }
 
 void loop() {
-  Serial.printf("Run time = %ld, ASCII value = %d, chacter = %c\n", millis(), ch, ch);
+  Serial.printf("Run time = %ld, ASCII value = %d, character = %c\n", millis(), ch, ch);
+  serialPrint();
   ch++;
   if (ch > 90) ch = 65;
 
   delay(1000);
+}
+
+void serialPrint() {
+  Serial.print("Run time = "); Serial.print(millis()); Serial.print(", ");
+  Serial.print("ASCII value = "); Serial.print(ch); Serial.print(", ");
+  Serial.print("character = "); Serial.write(ch); Serial.println("");
 }
